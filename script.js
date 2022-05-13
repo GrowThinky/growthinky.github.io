125c952d97d92ea534070ab170e1948d6cab2f53
GrowThinky <30656923+GrowThinky@users.noreply.github.com>
2022-05-14 00:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var speed0 = 20;
 
 
 (function () {
-  document.body.style.zoom="78%";
+  document.body.style.zoom="90%";
   setModeEventListener();
   setRandomLinkColor();
   setColorHoverListener();
